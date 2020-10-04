8fb795e7482998ab512f2b112f382c7498d069d5
aishwarya14m <aishwarya14m@gmail.com>
2020-10-04 20:10:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function myCheckFunction() {
+    var checkBox = document.getElementById("myCheck");
+    var text = document.getElementById("text");
+    if (checkBox.checked == true){
+      text.style.display = "block";
+    } else {
+       text.style.display = "none";
+    }
+  }

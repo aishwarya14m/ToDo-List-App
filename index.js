0959f9ad58c8992720e905ca4238f19be0f31047
aishwarya14m <aishwarya14m@gmail.com>
2020-10-04 20:21:17 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-function myCheckFunction() {
-    var checkBox = document.getElementById("myCheck");
-    var text = document.getElementById("text");
-    if (checkBox.checked == true){
-      text.style.display = "block";
-    } else {
-       text.style.display = "none";
-    }
-  }
